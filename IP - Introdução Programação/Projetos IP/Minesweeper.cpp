6dd5e064ad5e3bb550d464728ef08587249148a1
André Rosa (Vivobook) <andre.reddrain@gmail.com>
2025-10-06 18:29:40 +0100

    Minesweeper - Enunciado

---
+++ IP - Introdução Programação/Projetos IP/Minesweeper.cpp
@@ -0,0 +1,34 @@
+/* Jogo do Minesweeper - Enunciado
+
+Representação visual
+- X representa uma mina
+- O significa que não há mina
+
+Representação "informática"
+- O campo de minas é um array de booleanos
+- Uma mina significa "true"
+
+Dimensão do campo
+- Uma linha com maxCol colunas
+
+Funcionalidades (cada uma feita com uma função)
+- listaMinas => mostra o campo no ecrã (ex. para 8 casas X00XX0X0)
+- limpaMinas => remove todas as minas do tabuleiro de jogo
+- plantaMinas => pergunta ao utilizador quantas minas quer plantar e onde deve ficar cada uma delas
+- jogar => recebe o array e a respetiva dimensão
+    - pergunta em que casa quer jogar
+    - devolve false se acertou numa mina
+
+Programa principal
+- Cria o campo vazio
+    - Limpa-o
+    - Permite colocar as minas
+- Deixa o jogador jogar até acertar numa mina
+- Mostra o campo
+- Diz qual a pontuação obtida (quantas jogadas fez até acertar na mina)
+*/
+
+int main()
+{
+    //TODO Minesweeper
+}
